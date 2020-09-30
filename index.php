fceb8b3aeac6b5550f98a28f9bc1044666e72118
Zvi Epner <zepner@gmail.com>
2020-09-30 11:03:11 -0600

    Caching.

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+$lastModified=filemtime(__FILE__);
+$etagFile = md5_file(__FILE__);
+$ifModifiedSince=(isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false);
+$etagHeader=(isset($_SERVER['HTTP_IF_NONE_MATCH']) ? trim($_SERVER['HTTP_IF_NONE_MATCH']) : false);
+header("Last-Modified: ".gmdate("D, d M Y H:i:s", $lastModified)." GMT");
+header("Etag: $etagFile");
+header('Cache-Control: public');
+if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE'])==$lastModified || $etagHeader == $etagFile) {
+       header("HTTP/1.1 304 Not Modified");
+       exit;
+}
+//echo "This page was last modified: ".date("d.m.Y H:i:s",time());
 include_once('functions.php');
 $page_uri = ltrim($_SERVER['REQUEST_URI'], '/');
 $page = (strlen($page_uri) == 0) ? 'home' : $page_uri;
