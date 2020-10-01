9ba03c3bbe1d4fce8afb65328f11930abb70486a
Zvi Epner <zepner@gmail.com>
2020-10-01 12:54:08 -0600

    Method page.

---
+++ settings-methodology.php
@@ -0,0 +1,150 @@
+<?php
+$sections = [
+	'settings' => [
+	],
+	'lead' => [
+		'settings' => [
+			'class' => 'brown'
+		],
+		'h1' => [
+			'attrs' => [
+				'rel' => 'methodology:lead:h1'
+			],
+			'value' => 'Our Approach'
+		],
+		'h4' => [
+			'attrs' => [
+				'rel' => 'methodology:lead:h4'
+			],
+			'value' => 'Custom software built around people and the work they do. Organic improvements that can be implemented in a fraction of the time compared to mega-tech ERP providers. <br><br>No licensing fees. <br>Flexible framework. <br>Lasting competitive edge.'
+		],
+		'p' => [
+			'attrs' => [
+				'rel' => 'methodology:lead:cta',
+				'class' => 'cta'
+			],
+			'value' => 'Read more below&hellip;'
+		]
+	],
+	'step1' => [
+		'settings' => [
+			'class' => 'white steps nopadbottom'
+		],
+		'h6' => [
+			'attrs' => [
+				'rel' => 'methodology:step1:step-1',
+				'class' => 'red'
+			],
+			'value' => 'Step 1'
+		],
+		'h2' => [
+			'attrs' => [
+				'rel' => 'methodology:step1:h2'
+			],
+			'value' => 'Operations Assessment'
+		],
+		'p' => [
+			'attrs' => [
+				'rel' => 'methodology:step1:p'
+			],
+			'value' => 'Don\'t wait to increase cash flow &amp; productivity. <br>You\'ve got operations bottlenecks that can be solved today with a good hard look and a little code. <strong>OpLogic Diagnostics</strong> will help measure the gains that can be acheieved versus the cost to automate.'
+		]
+	],
+	'step2' => [
+		'settings' => [
+			'class' => 'white steps'
+		],
+		'h6' => [
+			'attrs' => [
+				'rel' => 'methodology:step2:step-2',
+				'class' => 'red'
+			],
+			'value' => 'Step 2'
+		],
+		'h2' => [
+			'attrs' => [
+				'rel' => 'methodology:step2:h2'
+			],
+			'value' => 'Critical Repairs'
+		],
+		'p' => [
+			'attrs' => [
+				'rel' => 'methodology:step2:p'
+			],
+			'value' => 'Where are you losing cash? <br><br>Many companies try to solve these complex operational issues with a shiny new expensive system.  Instead, our approach starts with focus on building or repairing foundational systems that <strong>directly improve daily operations</strong>.'
+		]
+	],
+	'step3' => [
+		'settings' => [
+			'class' => 'brown steps nopadbottom'
+		],
+		'h6' => [
+			'attrs' => [
+				'rel' => 'methodology:step3:step-3',
+				'class' => 'red'
+			],
+			'value' => 'Step 3'
+		],
+		'h2' => [
+			'attrs' => [
+				'rel' => 'methodology:step3:h2'
+			],
+			'value' => 'Business Logic Mapping'
+		],
+		'p' => [
+			'attrs' => [
+				'rel' => 'methodology:step3:p'
+			],
+			'value' => 'How much time and money is being wasted on guesswork? <br><br>Digitally mapping production from raw inputs to delivery at every point enables clarity about opportunities for documentation and/or automation.'
+		]
+	],
+	'step4' => [
+		'settings' => [
+			'class' => 'brown steps'
+		],
+		'h6' => [
+			'attrs' => [
+				'rel' => 'methodology:step4:step-4',
+				'class' => 'red'
+			],
+			'value' => 'Step 4'
+		],
+		'h2' => [
+			'attrs' => [
+				'rel' => 'methodology:step4:h2'
+			],
+			'value' => 'Rapid Software Deployment'
+		],
+		'p' => [
+			'attrs' => [
+				'rel' => 'methodology:step4:p'
+			],
+			'value' => 'Spending months "architecting" a large system? Not here. <br><br>OpLogic builds software that improves floor operations while also providing real-time activity reporting. Constantly improving the landscape of how you do business, we iterate often so you can get the benefit of new tools integrated faster.'
+		]
+	],
+	'step5' => [
+		'settings' => [
+			'class' => 'white steps'
+		],
+		'h6' => [
+			'attrs' => [
+				'rel' => 'methodology:step5:step-5',
+				'class' => 'red'
+			],
+			'value' => 'Step 5'
+		],
+		'h2' => [
+			'attrs' => [
+				'rel' => 'methodology:step5:h2',
+				'class' => 'red'
+			],
+			'value' => 'Scalability'
+		],
+		'p' => [
+			'attrs' => [
+				'rel' => 'methodology:step5:p'
+			],
+			'value' => 'Automation is only effective when processes are well-understood and defined in detail. <br><br>OpLogic diligently draws out every step within every workflow to build a custom system built on the elements that make your business work.'
+		]
+	]
+];
